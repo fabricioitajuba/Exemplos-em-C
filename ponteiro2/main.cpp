0246083fafbe57800f304afe79752a7a004790b6
Fabrício de Lima Ribeiro <31161759+fabricioitajuba@users.noreply.github.com>
2018-11-17 20:01:46 -0200

    Add files via upload

---
+++ ponteiro2/main.cpp
@@ -0,0 +1,28 @@
+//Exemplo de estruturas que contém ponteiros
+//Eng. Fabrício de Lima Ribeiro
+//17-11-18
+#include <stdio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void) {
+	
+	struct horario{
+		int *pHora;
+		int *pMinuto;
+		int *pSegundo;
+	};
+	struct horario hoje;
+	
+	int hora = 20;
+	int minuto = 30;
+	int segundo = 40;
+	
+	hoje.pHora = &hora;
+	hoje.pMinuto = &minuto;
+	hoje.pSegundo = &segundo;
+	
+	printf("%i:%i:%i", *hoje.pHora, *hoje.pMinuto, *hoje.pSegundo);
+	
+	return 0;
+}
